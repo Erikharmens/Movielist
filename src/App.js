db1cab3d1fc1a2d5e4bc3cabd9a10c9ca752d847
Erikharmens <54328933+Erikharmens@users.noreply.github.com>
2022-04-18 21:50:27 +0200

    add to watchlist enabled w localstorage

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import { Watched } from "./components/Watched";
 import { Add } from "./components/Add";
 import "./App.css";
 
+import { GlobalProvider } from "./context/GlobalState";
+
 // Tutorial - https://www.youtube.com/watch?v=1eO_hNYzaSc //
 
 function App() {
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Watchlist />} />
-        <Route path="/watched" element={<Watched />} />
-        <Route path="/add" element={<Add />} />
-      </Routes>
-    </Router>
+      <GlobalProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Watchlist />} />
+            <Route path="/watched" element={<Watched />} />
+            <Route path="/add" element={<Add />} />
+          </Routes>
+        </Router>
+      </GlobalProvider>
     </>
   );
 }
